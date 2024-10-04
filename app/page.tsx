e2f03deba72ed1ca49ed5ef516b74ce2ef69a97c
Lincoln Souza <lincolncloud23@gmail.com>
2024-10-04 11:15:12 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,9 @@
-"use client" //torna essa página uma client componente, ou seja, o que tiver de js vai ser renderizado, coisa que por padrão a page.tsx (server component) não faz
+//"use client" //torna essa página uma client componente, ou seja, o que tiver de js vai ser renderizado, coisa que por padrão a page.tsx (server component) não faz
 
-import { useState } from "react"
+import { Button } from "./_components/ui/button"
 
 const Home = () => {
-  const [] = useState()
-  return <h1 className="mb-5 text-clip bg-red-400 text-green-500">TESTE</h1>
+  return <Button>Teste</Button>
 }
 
 export default Home
