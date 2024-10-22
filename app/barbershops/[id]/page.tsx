6e0c302d43b53aef4700779f0d31aabf06311917
Lincoln Souza <lincolncloud23@gmail.com>
2024-10-22 20:17:37 -0300

    fix: correction bugs

--- app/barbershops/[id]/page.tsx
+++ app/barbershops/[id]/page.tsx
@@ -1,20 +1,13 @@
-<<<<<<< HEAD
-import { Button } from "@/app/_components/ui/button"
-=======
 import PhoneItem from "@/app/_components/phone-item"
 import ServiceItem from "@/app/_components/service-item"
 import SidebarSheet from "@/app/_components/sidebar-sheet"
 import { Button } from "@/app/_components/ui/button"
 import { Sheet, SheetTrigger } from "@/app/_components/ui/sheet"
->>>>>>> 42b72b1
 import { db } from "@/app/_lib/prisma"
 import { ChevronLeftIcon, MapPinIcon, MenuIcon, StarIcon } from "lucide-react"
 import Image from "next/image"
 import Link from "next/link"
-<<<<<<< HEAD
-=======
 import { notFound } from "next/navigation"
->>>>>>> 42b72b1
 
 interface BarbershopPageProps {
   params: {
@@ -32,14 +25,11 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
       services: true,
     },
   })
-<<<<<<< HEAD
-=======
 
   if (!barbershop) {
     return notFound()
   }
 
->>>>>>> 42b72b1
   return (
     <div>
       {/* Image */}
@@ -62,15 +52,6 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           </Link>
         </Button>
 
-<<<<<<< HEAD
-        <Button
-          size="icon"
-          variant="secondary"
-          className="absolute right-4 top-4"
-        >
-          <MenuIcon />
-        </Button>
-=======
         <Sheet>
           <SheetTrigger asChild>
             <Button
@@ -83,26 +64,17 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           </SheetTrigger>
           <SidebarSheet />
         </Sheet>
->>>>>>> 42b72b1
       </div>
 
       {/*Top Head  */}
       <div className="border-b border-solid p-5">
         <h1 className="mb-3 text-xl font-bold">{barbershop?.name}</h1>
-<<<<<<< HEAD
-        <div className="mb-2 flex items-center gap-1">
-=======
         <div className="mb-2 flex items-center gap-2">
->>>>>>> 42b72b1
           <MapPinIcon className="text-primary" size={18} />
           <p className="text-sm">{barbershop?.address}</p>
         </div>
 
-<<<<<<< HEAD
-        <div className="mb-2 flex items-center gap-1">
-=======
         <div className="mb-2 flex items-center gap-2">
->>>>>>> 42b72b1
           <StarIcon className="text-primary" size={18} />
           <p className="text-sm">5,0 (434 avaliações)</p>
         </div>
@@ -113,8 +85,6 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
         <h2 className="text-xs font-bold uppercase text-gray-400">Sobre nós</h2>
         <p className="text-justify text-sm">{barbershop?.description}</p>
       </div>
-<<<<<<< HEAD
-=======
 
       {/* Services */}
       <div className="space-y-3 border-b border-solid p-5">
@@ -136,7 +106,6 @@ const BarbershopPage = async ({ params }: BarbershopPageProps) => {
           <PhoneItem key={phone} phone={phone} />
         ))}
       </div>
->>>>>>> 42b72b1
     </div>
   )
 }
